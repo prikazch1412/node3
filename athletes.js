57daacc2fa563e7a899f890b7b80f402beb77fbb
tenda150 <45257694+tenda150@users.noreply.github.com>
2018-12-17 20:14:39 +0200

    Update athletes.js

--- athletes.js
+++ athletes.js
@@ -1,15 +1,12 @@
 var fast_csv = require('fast-csv');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('olympic_history.db');
-
 var tempArray = new Array();
 
-fast_csv.fromPath("athlete.csv").on("data", function(data){
+fast_csv.fromPath("athlete3.csv").on("data", function(data){
     str = data[0].replace(/""/g,'').replace(/(\(.*?\))/g, '');
-    tempArray.push(str.match(/(?!\,\")([\&\`\#\/\.\'\d\w\s,-]+)/ig));
-    
-})
-  .on("end", function(){
+    tempArray.push(str.match(/(?!\,\")([\&\`\#\/\.\'\d\w\s,-]+)/ig));   
+}) .on("end", function(){
     tempArray.shift();
     data(tempArray);
 });
@@ -17,29 +14,18 @@ fast_csv.fromPath("athlete.csv").on("data", function(data){
 function data(arr) {
     var age_heigh_weig = [];
     var str2 =[];
-    var arr3 = [];
-    var id = [];
-    var noc_name = [];
-    var str3 = [];
-    
     db.serialize(function() {
+        // db.run("drop table athletes");
+        // db.run("CREATE TABLE `athletes` (`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `full_name`	TEXT NOT NULL,`year_of_birth`	INTEGER,`sex`	INTEGER,`params`	TEXT NOT NULL,`team_id`	INTEGER NOT NULL);");
         var stmt = db.prepare("insert into athletes values(null,?,?,?,?,?)");
-        db.each("select * from teams",function(err,row) {
-        
-           
-            id.push(row.id);
-            noc_name.push(row.noc_name);
-           //console.log(row.id);
-    for (let i=0; i<id.length;i++){
-        arr3[i] = Array.of (id[i], noc_name[i]);
-    //console.log(arr3[i][0]);
-    }
+    let sql = `select * from teams`;
+            
+db.all(sql, [], (err, rows) => {  if (err) { throw err; }
     for (let i=0; i<arr.length; i++){
-        age_heigh_weig[i] = arr[i][3].substring(1, arr[i][3].length-1);
-        for (let i=0;i<age_heigh_weig.length;i++){
-            str2[i] = age_heigh_weig[i].split(',');
-          // console.log(str2[i])
-        }
+            age_heigh_weig[i] = arr[i][3].substring(1, arr[i][3].length-1);
+            for (let i=0;i<age_heigh_weig.length;i++){
+                str2[i] = age_heigh_weig[i].split(',');           
+            }
         for(let i = 0; i < str2.length; i++) {
             if(str2[i][1] == 'NA' && str2[i][2] == 'NA') {
                 var hw = {};
@@ -51,18 +37,16 @@ function data(arr) {
                 var hw = {height: str2[i][1], weight: str2[i][2]};
             }     
         }
-        for (let j=0; j<arr3.length; j++) { 
-        if (arr[i][5] == arr3[j][1]){ 
-            console.log(arr[i][1], arr[i][2], str2[i][0], JSON.stringify(hw), arr3[j][0]);
-             
-                }
+        for (let j in rows) { 
+        if (arr[i][5] == rows[j].noc_name){ 
+            stmt.run(arr[i][1], str2[i][0], arr[i][2],  JSON.stringify(hw), rows[j].id);
         }
-    }
-        });
-        
-        
-
-    stmt.finalize();                 
+    } 
+}    
+stmt.finalize();
+});                
     });
-    db.close();
+    db.close(function(){
+        console.log("Запись в БД окончена")
+    });
 }
